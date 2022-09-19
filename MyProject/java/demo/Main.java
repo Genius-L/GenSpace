564a6ca4150b4a6c6ba4c447de3ad046d01a36f6
genRogStrix <geniuslreturn@gmail.com>
2022-09-19 22:04:24 +0000

    Java TP

---
+++ MyProject/java/demo/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        int age = 55;
+        boolean isEch = age == 60 ? true : false ;
+        System.out.println(":::: isEch : "+isEch);
+
+    }
+
+}
