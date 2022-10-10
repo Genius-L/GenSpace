43d766a980831fbdae48a8bd50da8459627b7451
genRogStrix <geniuslreturn@gmail.com>
2022-10-10 11:40:56 +0100

    java TP

--- MyProject/java/demo/Main.java
+++ MyProject/java/demo/Main.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+
 /**
  * Main
  */
@@ -5,9 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int age = 55;
-        boolean isEch = age == 60 ? true : false ;
-        System.out.println(":::: isEch : "+isEch);
 
     }
 
